255c61affd4aebb118c002899ef60c7fb4e949b1
ybdesire <ybdesire@gmail.com>
2015-03-06 02:44:06 -0500

    over!

--- vj.py
+++ vj.py
@@ -4,21 +4,27 @@ import codecs
 import collections
 from xml.sax.saxutils import escape
 
+#python 3.4.3
 # scan current dir and detect if all *.json file are structure valid
 def main():
-    print(os.getcwd())
+	for root,dirs,files in os.walk(os.getcwd()):
+		for fileName in files:
+			if os.path.splitext(fileName)[1] == '.json':
+				jsonFilePath = root+'\\'+fileName
+				if(isJson(jsonFilePath)):
+					print("VALID: " + fileName)
 
 #validate if a json file is correct
 def isJson(filePath):
-    try:
-        fileData = codecs.open(filePath, 'r', 'utf-8-sig').read()
-        data = json.loads(fileData)
-    except ValueError as e:
-        print('JSON FILE ERROR: ' + filePath)
-        print(ValueError)
-        print(e)
-        return False
-    return True
+	try:
+		fileData = codecs.open(filePath, 'r', 'utf-8-sig').read()
+		data = json.loads(fileData)
+	except ValueError as e:
+		print('JSON FILE ERROR: ' + filePath)
+		print(ValueError)
+		print(e)
+		return False
+	return True
 
 if __name__=='__main__':
     main()
